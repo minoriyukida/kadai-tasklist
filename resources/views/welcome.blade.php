d3c06066551568ccca490136c7a619d82995a446
yukidaminori <rinngo903@gmail.com>
2020-05-07 07:17:22 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -89,6 +89,17 @@
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
+                
+                @extends('layouts.app')
+
+                @section('content')
+                    <div class="center jumbotron">
+                        <div class="text-center">
+                            <h1>Welcome to the Microposts</h1>
+                            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+                        </div>
+                    </div>
+                @endsection
             </div>
         </div>
     </body>
